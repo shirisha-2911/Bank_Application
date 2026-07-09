f9a44eb64567a604f10fad89bed69b699c7e9da2
shirisha-2911 <Shirisharigela@gmail.com>
2026-07-09 17:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ accounts = st.session_state.accounts
 # Title
 # ==========================================
 
-st.title("Shirisha:Heart: Smart Banking Management System")
+st.title("Shirisha Smart Banking Management System")
 
 st.write(
     "Manage customer accounts using a simple Streamlit application."
